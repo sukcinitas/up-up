6ced41827bf35c8f0d837e68f2c7a519ffb287d1
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2020-10-08 20:18:02 +0300

    Pepper passwords

--- src/server/passwordHashing.ts
+++ src/server/passwordHashing.ts
@@ -1,9 +1,13 @@
 import { compareSync, hashSync } from 'bcryptjs';
 
+const pepperCode = '7510016265 7204971188';
+const pepperEnv = process.env.PEPPER;
+const pepper = `${pepperCode}${pepperEnv}`;
+
 export function hashPassword(password: string): string {
-  return hashSync(password, 10);
+  return hashSync(`${password}${pepper}`, 10);
 }
 
 export function comparePassword(assumedPassword: string, hashedPassword: string): boolean {
-  return compareSync(assumedPassword, hashedPassword);
+  return compareSync(`${assumedPassword}${pepper}`, hashedPassword);
 }
