ff8483b6bafb669bc01be2b69f67f6cba32378c6
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2024-05-26 14:35:36 +0300

    Update webpack configuration

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -47,11 +47,13 @@ export default {
       },
       {
         test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
         use: ['babel-loader', 'eslint-loader'],
       },
       {
         test: /\.(ts|tsx)$/,
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
